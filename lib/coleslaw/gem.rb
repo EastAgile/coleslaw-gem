96c4702596bed8c20e1e90ed0c5f5b9ffdb2195f
Phuong Nguyen+Bang Dao <phuongnd08@gmail.com>
2013-11-12 15:40:27 +0700

    Initial code for coleslaw-gem

---
+++ lib/coleslaw/gem.rb
@@ -0,0 +1,7 @@
+require "coleslaw/gem/version"
+
+module Coleslaw
+  module Gem
+    # Your code goes here...
+  end
+end
---
+++ lib/coleslaw/gem/version.rb
@@ -0,0 +1,5 @@
+module Coleslaw
+  module Gem
+    VERSION = "0.0.1"
+  end
+end
